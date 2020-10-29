cdcaeaf72b362b19581729b9bdbe69532ea67379
mattuzi17 <quilaym@gmail.com>
2020-10-29 14:36:09 -0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-// @TODO: YOUR CODE HERE!
+// plot dimensions
+var svgWidth = 960; 
+var svgHeight = 910; 
+
+var margin = {
+  top: 20,
+  right: 40, 
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// create SVG wrapper
+
